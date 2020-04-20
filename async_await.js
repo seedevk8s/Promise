98e09fe5091531db5e079eac75cb8bc65a1d2f0a
chjin <seedevk8s@gmail.com>
2020-04-20 20:32:17 +0900

    async 함수에서 에러를 발생 시킬때에는 throw 를 사용하고, 에러를 잡아낼 때에는 try/catch 문을 사용합니다.

--- async_await.js
+++ async_await.js
@@ -7,13 +7,30 @@ function sleep(ms) {
 /*함수를 선언 할 때 함수의 앞부분에 async 키워드를 붙여주세요.*/
 /* Promise 의 앞부분에 await 을 넣어주면 해당 프로미스가 끝날때까지 기다렸다가 다음 작업을 수행 할 수 있습니다.*/
 /* sleep 이라는 함수를 만들어서 파라미터로 넣어준 시간 만큼 기다리는 Promise 를 만듦*/
-async function process() {
-    console.log('안녕하세요!');
+
+/* async 함수에서 에러를 발생 시킬때에는 throw 를 사용하고, 에러를 잡아낼 때에는 try/catch 문을 사용합니다.*/
+async function makeError() {
     await sleep(1000);    //1초 멈추고
-    console.log('반갑습니다!');
+    const error = new Error();
+    throw error;
 }
 
-/* 함수에서 async 를 사용하면, 해당 함수는 결과값으로 Promise 를 반환하게 됩니다.*/
-process().then(() => {
-    console.log('작업이 끝났어요!');
-});
+async function process() {
+    try {
+        await makeError();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+process();
+
+
+
+
+
+
+
+
+
+
