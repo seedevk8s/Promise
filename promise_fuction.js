2cfd07a9b970d8cff0cd16afa4014b2751fee0f3
chjin <seedevk8s@gmail.com>
2020-04-20 16:44:29 +0900

    Promise 를 사용하면, 비동기 작업의 개수가 많아져도 코드의 깊이가 깊어지지 않게 됩니다

--- promise_fuction.js
+++ promise_fuction.js
@@ -15,6 +15,11 @@ function increaseAndPrint(n) {
     });
 }
 
+/*Promise 를 사용하면, 비동기 작업의 개수가 많아져도 코드의 깊이가 깊어지지 않게 됩니다.*/
+/*이것도 불편한점이 있긴 합니다.
+에러를 잡을 때 몇번째에서 발생했는지 알아내기도 어렵고
+특정 조건에 따라 분기를 나누는 작업도 어렵고,
+특정 값을 공유해가면서 작업을 처리하기도 까다롭습니다.*/
 increaseAndPrint(0)
     .then(increaseAndPrint)
     .then(increaseAndPrint)
