48e873f6e758404b02f5c08f678e9d32380035c3
chjin <seedevk8s@gmail.com>
2020-04-20 16:22:26 +0900

    Promise 에서 실행 실패시 경우

--- promise.js
+++ promise.js
@@ -7,12 +7,21 @@
 
 /*resolve 를 호출 할 때 특정 값을 파라미터로 넣어주면, 이 값을 작업이 끝나고 나서 사용 할 수 있습니다.
 작업이 끝나고 나서 또 다른 작업을 해야 할 때에는 Promise 뒤에 .then(...) 을 붙여서 사용하면 됩니다.*/
+
+/*이번에는, 1초뒤에 실패되게끔 해봅시다.*/
+/*실패하는 상황에서는 reject 를 사용하고,
+.catch 를 통하여 실패했을시 수행 할 작업을 설정 할 수 있습니다.*/
 const myPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve(1);
+        reject(new Error())
     }, 1000);
 });
 
-myPromise.then(n => {
-    console.log(n);
-});
+myPromise
+    .then(n => {
+        console.log(n);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
